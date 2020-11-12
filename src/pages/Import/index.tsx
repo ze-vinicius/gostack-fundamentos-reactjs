96c5ce2bffdf2bc334b2c031fc8407f92d318b46
Vinicius Barcelos <josevsaraiva@gmail.com>
2020-11-12 19:34:18 -0300

    feat: implementar funcionalidades do desafio

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
-import filesize from 'filesize';
+import Filesize from 'filesize';
 
 import Header from '../../components/Header';
 import FileList from '../../components/FileList';
@@ -23,19 +23,29 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
     // TODO
 
+    data.append('file', uploadedFiles[0].file, uploadedFiles[0].name);
+
     try {
-      // await api.post('/transactions/import', data);
+      await api.post('/transactions/import', data);
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    //
+
+    const formatFiles = files.map(file => ({
+      file,
+      name: file.name,
+      readableSize: Filesize(file.size),
+    }));
+
+    setUploadedFiles(formatFiles);
   }
 
   return (
